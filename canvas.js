b98abbc35dcc019b901581dca61cb38f3decc64c
Mitja Semolic <mitja@Mitjas-MacBook-Pro.local>
2017-12-04 00:10:18 +0100

    draw gauge

--- canvas.js
+++ canvas.js
@@ -4,4 +4,28 @@ window.onload = function() {
 
     var width = canvas.width
     var height = canvas.height
+
+    var degrees = 200;
+    var color = "lightgreen";
+    var backgroundColor = "#222";
+
+    function setup() 
+    {
+        // background circle
+        g.beginPath();
+        g.strokeStyle = backgroundColor;
+        g.lineWidth = 30;
+        g.arc(width/2, height/2, 100, 0, 2*Math.PI, false);
+        g.stroke();
+
+        // gauge will be a simple arc
+        var radians = degrees * Math.PI / 180;
+        g.beginPath();
+        g.strokeStyle = color;
+        g.lineWidth = 30;
+        g.arc(width/2, height/2, 100, -0.5*Math.PI, radians -0.5*Math.PI, false);
+        g.stroke();
+    }
+
+    setup();
 }
