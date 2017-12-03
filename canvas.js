0a3f4dc22baf97bfd0df768c2b3ad0b225460494
Mitja Semolic <mitja@Mitjas-MacBook-Pro.local>
2017-12-03 22:47:31 +0100

    canvas boilerplate

---
+++ canvas.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var g = canvas.getContext("2d");
+
+    var width = canvas.width
+    var height = canvas.height
+}
